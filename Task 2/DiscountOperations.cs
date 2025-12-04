9843ba7f632961c2339ee3bf7d63e7849b08f779
CURDMaster24 <bishwasroshan11@gmail.com>
2025-12-04 11:39:35 +0545

    Task complete

--- Task 2/DiscountOperations.cs
+++ Task 2/DiscountOperations.cs
@@ -2,18 +2,16 @@
 
 namespace Task2
 {
-    // creating delegate that takes a price and returns discounted price
     public delegate double DiscountStrategy(double price);
 
     public class DiscountOperations
     {
-        // now giving 20% discount
         public double FestivalDiscount(double price)
         {
             return price * 0.80;
         }
 
-        // now giving 10% discount
+      
         public double SeasonalDiscount(double price)
         {
             return price * 0.90;
@@ -25,7 +23,7 @@ namespace Task2
             return price;
         }
 
-        // now calculating final price using delegate strategy
+        
         public double CalculateFinalPrice(double originalPrice, DiscountStrategy strategy)
         {
             return strategy(originalPrice);
---
+++ Task 6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TravelCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the sample bookings
+            List<TourBooking> bookings = new List<TourBooking>()
+            {
+                new TourBooking("Roshan Bishwas Tharu", "Kathmandu", 12000, 5, false),
+                new TourBooking("Rajaram Bishwas", "America", 8000, 3, false),
+                new TourBooking("Ritik Bishwas ", "Paris", 50000, 7, true),
+                new TourBooking("Uday Bishwas", "Lumbini", 15000, 6, false),
+                new TourBooking("Sneha Bishwas", "London", 45000, 5, true)
+            };
+
+            // LINQ - Filter, Transform, Sort
+            var tourSummaries = bookings
+                .Where(t => t.Price > 10000 && t.DurationInDay > 4)
+                .Select(t => new TourSummary(
+                    t.CustomerName,
+                    t.Destination,
+                    t.IsInternational ? "International" : "Domestic",
+                    t.Price))
+                .OrderBy(t => t.Category)
+                .ThenBy(t => t.Price)
+                .ToList();
+
+            // Now Displaying the results....
+            Console.WriteLine("Filtered and Sorted Tour Bookings:\n");
+
+            foreach (var tour in tourSummaries)
+            {
+                tour.Display();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
